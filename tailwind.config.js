c0dffafb279ac021643fb8eff8f4c4ca513680e5
hritikcoder <92904159+hritikcoder@users.noreply.github.com>
2023-12-30 15:07:52 +0530

    Uploded all files
    Uploded all files of Razorpayclone website

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mullish: ["Mulish", "sans-serif"],
+      },
+      colors: {
+        deepBlue: "#02042a",
+        lightBlue: "#2b84ea",
+        lightBlue300: "#4b94ed",
+        greenLight: "#61cea6",
+        graytext: "#818597",
+        lightGray: "#e2e2e2",
+        grayBlue: "#344a6c",
+        deepBlue: "#162f56",
+        gray2: "#525a76",
+        blue1: "rgb(4 3 45)"
+      }
+    },
+  },
+  plugins: [],
+}
+
